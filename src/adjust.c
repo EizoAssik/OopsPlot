9b569b9fc8073f542f8a3b3428b7f9c5a01f15b9
NerdWizard <eizoassik@gmail.com>
2014-10-27 19:32:12 +0800

    修复旋转

--- src/adjust.c
+++ src/adjust.c
@@ -12,13 +12,15 @@ void update_setting(ADJUST_SETTING * setting) {
 
 void adjust(double x, double y, Point * desc,
             ADJUST_SETTING * setting) {
+    double nx, ny;
     if (*scl_changed) {
         x *= setting->scale_x;
         y *= setting->scale_y;
     }
+    nx = x; ny = y;
     if (*rot_changed) {
-        x  = setting->cos_rot * x + y * setting->sin_rot;
-        y  = setting->cos_rot * y - x * setting->sin_rot;
+        x  = setting->cos_rot * nx + ny * setting->sin_rot;
+        y  = setting->cos_rot * ny - nx * setting->sin_rot;
     }
     if (*org_changed) {
         x += setting->offset_x;
