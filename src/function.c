f12a4c84689188159ade5998ef4abaa342267618
NerdWizard <eizoassik@gmail.com>
2014-11-26 23:45:25 +0800

    添加空格以对齐

--- src/function.c
+++ src/function.c
@@ -25,17 +25,17 @@ double minus(double x) {
 }
 
 FUNC_INFO BUILTIN_FUNC[] = {
-    {.name = "(+)",   .argc = 1, .func = plus},
-    {.name = "(-)",   .argc = 1, .func = minus},
-    {.name = "SIN", .argc = 1, .func = sin},
-    {.name = "COS", .argc = 1, .func = cos},
-    {.name = "TAN", .argc = 1, .func = tan},
-    {.name = "SART",.argc = 1, .func = sqrt},
-    {.name = "+",   .argc = 2, .func = add},
-    {.name = "-",   .argc = 2, .func = sub},
-    {.name = "*",   .argc = 2, .func = mul},
-    {.name = "/",   .argc = 2, .func = divsion},
-    {.name = "**",  .argc = 2, .func = pow},
+    { .name = "(+)",   .argc = 1, .func = plus    },
+    { .name = "(-)",   .argc = 1, .func = minus   },
+    { .name = "SIN",   .argc = 1, .func = sin     },
+    { .name = "COS",   .argc = 1, .func = cos     },
+    { .name = "TAN",   .argc = 1, .func = tan     },
+    { .name = "SART",  .argc = 1, .func = sqrt    },
+    { .name = "+",     .argc = 2, .func = add     },
+    { .name = "-",     .argc = 2, .func = sub     },
+    { .name = "*",     .argc = 2, .func = mul     },
+    { .name = "/",     .argc = 2, .func = divsion },
+    { .name = "**",    .argc = 2, .func = pow     },
     // VOID
-    {.name = NULL,  .argc = 0, .func = NULL}
+    { .name = NULL,  .argc = 0, .func = NULL}
 };
